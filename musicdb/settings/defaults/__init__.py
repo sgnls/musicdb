99d01313d41dd63515c7d45265cd4354ecc37f55
Chris Lamb <lamby@debian.org>
2012-01-25 00:35:46 +0000

    Oh, we kinda had it.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/settings/defaults/__init__.py
+++ musicdb/settings/defaults/__init__.py
@@ -63,7 +63,6 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
