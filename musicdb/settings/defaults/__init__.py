137b8c0b8f982abda09f90f2d844a53c8f640b23
Chris Lamb <lamby@debian.org>
2012-01-25 00:36:06 +0000

    Add messages framework.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/settings/defaults/__init__.py
+++ musicdb/settings/defaults/__init__.py
@@ -64,6 +64,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'musicdb.urls'
