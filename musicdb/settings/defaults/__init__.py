8c2fb9021da283dd491e5dad3ced7869a5ae1cab
Chris Lamb <lamby@debian.org>
2012-01-25 00:36:32 +0000

    Drop XFrameOptionsMiddleware.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/settings/defaults/__init__.py
+++ musicdb/settings/defaults/__init__.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
