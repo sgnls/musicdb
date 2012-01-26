e1f4bea5fbc7dbdc48cbef1ada135b60b9a6a23c
Chris Lamb <lamby@debian.org>
2012-01-26 15:51:20 +0000

    Use strings.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/nonclassical/fuse_urls.py
+++ musicdb/nonclassical/fuse_urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls.defaults import *
 
-import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.fuse_index),
-    url(r'^/(?P<dir_name>[^/]+)$', views.fuse_artist),
-    url(r'^/(?P<artist_dir_name>[^/]+)/(?P<dir_name>[^/]+)$', views.fuse_album),
-    url(r'^/(?P<artist_dir_name>[^/]+)/(?P<album_dir_name>[^/]+)/(?P<dir_name>[^/]+)$', views.fuse_track),
+urlpatterns = patterns('musicdb.nonclassical.views',
+    url(r'^$', 'fuse_index'),
+    url(r'^/(?P<dir_name>[^/]+)$', 'fuse_artist'),
+    url(r'^/(?P<artist_dir_name>[^/]+)/(?P<dir_name>[^/]+)$', 'fuse_album'),
+    url(r'^/(?P<artist_dir_name>[^/]+)/(?P<album_dir_name>[^/]+)/(?P<dir_name>[^/]+)$', 'fuse_track'),
 )
