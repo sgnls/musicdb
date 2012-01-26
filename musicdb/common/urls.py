c4136fc581b21442f9515a8371a9c31ada5ea5b3
Chris Lamb <lamby@debian.org>
2012-01-26 18:41:20 +0000

    Why is this everywhere? :)
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/common/urls.py
+++ musicdb/common/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls.defaults import *
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('musicdb.common.views',
     url(r'^play/(?P<music_file_id>\d+)$', 'play_music_file',
         name='play-music-file'),
