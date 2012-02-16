f49dc1a95cdde4f800fe4b572f89f8d61f45d4b1
Chris Lamb <lamby@debian.org>
2012-02-16 15:28:21 +0000

    Drop this import.

--- musicdb/nonclassical/views.py
+++ musicdb/nonclassical/views.py
@@ -3,8 +3,6 @@
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 
-from django_fuse import DirectoryResponse, SymlinkResponse
-
 from musicdb.utils.iter import chunk
 from musicdb.utils.http import XSPFResponse
 from musicdb.nonclassical.models import Artist, Album, CD, Track
