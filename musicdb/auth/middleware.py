941333e281b439d831ba55d527ab315870974e0f
Chris Lamb <lamby@debian.org>
2012-02-21 10:34:25 +0000

    Require login for everything.
    Signed-off-by: Chris Lamb <lamby@debian.org>

---
+++ musicdb/auth/middleware.py
@@ -0,0 +1,21 @@
+from django.shortcuts import redirect
+
+class RequireLoginMiddleware(object):
+    ALLOW = (
+        '/media/',
+        '/admin',
+        '/__debug__/',
+    )
+
+    def process_view(self, request, fn, *args, **kwargs):
+        for prefix in self.ALLOW:
+            if request.path.startswith(prefix):
+                return
+
+        if request.user.is_authenticated():
+            return
+
+        if getattr(fn, 'login_not_required', False):
+            return
+
+        return redirect('home')
--- musicdb/views.py
+++ musicdb/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from musicdb.auth.decorators import login_not_required
+
+@login_not_required
 def index(request):
     return render(request, 'index.html')
