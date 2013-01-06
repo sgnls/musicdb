9ce08657293540ae207ac3b344f05df28b3553dd
Chris Lamb <lamby@debian.org>
2013-01-06 21:22:57 +0000

    Nice to have settings context.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/settings/defaults/__init__.py
+++ musicdb/settings/defaults/__init__.py
@@ -141,6 +141,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
+    'musicdb.utils.context_processors.settings_context',
 )
 
 LOGIN_URL = '/login'
