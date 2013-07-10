e4cebf643c4bfc4d8ea8c4aff97efb2f2e122d22
Chris Lamb <lamby@debian.org>
2013-07-10 11:22:39 +0100

    Make the default postgres
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/settings/roles/local.py
+++ musicdb/settings/roles/local.py
@@ -5,8 +5,8 @@ ADMINS = ()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-        'NAME': 'musicdb.sqlite',             # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+        'NAME': 'musicdb',             # Or path to database file if using sqlite3.
         'USER': '',             # Not used with sqlite3.
         'PASSWORD': '',         # Not used with sqlite3.
         'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
