4fbcbb1e38b5cf21aa915ed353945c859e3cf668
Chris Lamb <chris@chris-lamb.co.uk>
2013-09-21 17:42:31 +0100

    Add django_yadt
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ contrib/django_yadt/utils.py
@@ -0,0 +1,44 @@
+import os
+
+from django.db import models
+from django.core.management.base import CommandError
+
+from .fields import IMAGE_VARIANTS
+
+def get_variant(app_label, model_name, field_name, variant_name):
+    model = models.get_model(app_label, model_name)
+
+    if model is None:
+        raise CommandError("%s.%s is not a valid model name" % (
+            app_label,
+            model_name,
+        ))
+
+    try:
+        field = getattr(model, field_name)
+    except AttributeError:
+        raise CommandError("%s.%s has no field %s" % (
+            app_label,
+            model_name,
+            field_name,
+        ))
+
+    try:
+        return getattr(field, variant_name)
+    except AttributeError:
+        raise CommandError("%s.%s.%s has no variant %s" % (
+            app_label,
+            model_name,
+            field_name,
+            variant_name,
+        ))
+
+def get_variant_from_path(path):
+    for variant in IMAGE_VARIANTS:
+        # Append '' so we don't accidentally match a prefix
+        dirname = os.path.join(variant.field.upload_to, variant.name, '')
+
+        if path.startswith(dirname):
+            return variant
+
+    return None
--- musicdb/settings/defaults/__init__.py
+++ musicdb/settings/defaults/__init__.py
@@ -93,6 +93,7 @@ INSTALLED_APPS = (
 
     'debug_toolbar',
     'django_extensions',
+    'django_yadt',
     'treebeard',
     'south',
 
