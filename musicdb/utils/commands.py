9948dfb5f3ad1c1c9f842c23e00e0341f7268932
Chris Lamb <chris@chris-lamb.co.uk>
2014-04-07 16:53:04 +0100

    Disable.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/utils/commands.py
+++ musicdb/utils/commands.py
@@ -25,6 +25,8 @@ TODO
 
 class AddFilesCommand(BaseCommand):
     def handle(self, *files, **options):
+        assert False, "needs to be ported to default_storage"
+
         self.options = options
 
         # Expand if we have specified a directory
