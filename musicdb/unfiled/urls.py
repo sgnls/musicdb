41e5487bf8bb25395432c96102a5edabc5227510
Chris Lamb <chris@chris-lamb.co.uk>
2014-04-21 23:28:13 +0100

    Make unfiled work. A bit.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/unfiled/urls.py
+++ musicdb/unfiled/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('musicdb.unfiled.views',
-    url(r'^unfiled/$', 'view',
+    url(r'^unfiled/(?P<path>.+/)?$', 'view',
         name='view'),
-    url(r'^unfiled/play$', 'play',
-        name='play'),
-    url(r'^unfiled/m3u$', 'm3u',
-        name='m3u'),
 )
--- musicdb/unfiled/views.py
+++ musicdb/unfiled/views.py
@@ -1,93 +1,34 @@
 import os
-import re
 
-from django.http import Http404
-from django.conf import settings
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.files.storage import default_storage
 
-from musicdb.utils.http import render_playlist
+PREFIX = 'unfiled/classical'
 
-from .utils import Track
-
-re_show_play = re.compile(r'\.(mp3|flac)$', re.IGNORECASE)
-
-def view(request):
-    from django.http import HttpResponse
-    return HttpResponse("Returning soon...")
+def view(request, path=None):
+    if path is None:
+        path = ''
 
     try:
-        rel_path = request.GET['x']
-        parent = os.path.dirname(rel_path)
-    except KeyError:
-        rel_path = ''
-        parent = None
+        dirnames, filenames = default_storage.listdir(
+            os.path.join(PREFIX, path),
+        )
+    except Exception, exc:
+        return HttpResponse("Exception when listing dir: %r" % exc)
 
-    abs_path = os.path.realpath(
-        os.path.join(settings.UNFILED_MEDIA_LOCATION, rel_path)
-    )
-
-    if not abs_path.startswith(settings.UNFILED_MEDIA_LOCATION):
-        raise Http404
-
-    if not os.path.isdir(abs_path):
-        raise Http404
-
-    entries = [{
+    dirnames = [{
         'name': x,
-        'isdir': os.path.isdir(os.path.join(abs_path, x)),
-        'abs_path': os.path.join(abs_path, x),
-        'rel_path': os.path.join(rel_path, x),
-        'show_play': bool(re_show_play.search(x)),
-    } for x in os.listdir(abs_path)]
+        'path': '%s/' % os.path.join(path, x),
+    } for x in dirnames]
 
-    entries.sort(key=lambda x: (not x['isdir'], x['name']))
+    filenames = [{
+        'url': default_storage.url(os.path.join(PREFIX, path, x)),
+        'name': x,
+    } for x in filenames]
 
     return render(request, 'unfiled/view.html', {
-        'parent': parent,
-        'entries': entries,
-        'rel_path': rel_path,
-        'show_parent': parent is not None,
+        'path': path,
+        'dirnames': dirnames,
+        'filenames': filenames,
     })
-
-def play(request):
-    try:
-        rel_path = request.GET['x']
-    except KeyError:
-        raise Http404
-
-    abs_path = os.path.realpath(
-        os.path.join(settings.UNFILED_MEDIA_LOCATION, rel_path)
-    )
-
-    if not os.path.exists(abs_path):
-        raise Http404
-
-    return render_playlist(
-        request,
-        [Track(rel_path)],
-        prefix=settings.UNFILED_MEDIA_LOCATION_HTTP,
-    )
-
-def m3u(request):
-    try:
-        rel_path = request.GET['x']
-    except KeyError:
-        raise Http404
-
-    abs_path = os.path.realpath(
-        os.path.join(settings.UNFILED_MEDIA_LOCATION, rel_path)
-    )
-
-    if not os.path.isdir(abs_path):
-        raise Http404
-
-    entries = [
-        Track(os.path.join(rel_path, x)) for x in sorted(os.listdir(abs_path))
-        if re_show_play.search(x)
-    ]
-
-    return render_playlist(
-        request,
-        entries,
-        prefix=settings.UNFILED_MEDIA_LOCATION_HTTP,
-    )
