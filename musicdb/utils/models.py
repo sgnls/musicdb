1b09d55e030ae1281411289ed5ce260ac3aed8a0
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-01 11:01:44 +0200

    Use a templatetag witha setting for font handling.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/utils/models.py
+++ musicdb/utils/models.py
@@ -1,3 +1,4 @@
 from django.template import add_to_builtins
 
+add_to_builtins('musicdb.utils.templatetags.fonts')
 add_to_builtins('musicdb.utils.templatetags.media')
---
+++ musicdb/utils/templatetags/fonts.py
@@ -0,0 +1,13 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+GOOGLE_FONT_TEMPLATE = '<link href="//fonts.googleapis.com/css?family=%s" rel="stylesheet">'
+
+@register.simple_tag
+def google_font(definition):
+    if not settings.FONTS_ENABLED:
+        return ''
+
+    return TEMPLATE % definition
