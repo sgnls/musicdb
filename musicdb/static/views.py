941759814976f8bc5a48107ec2b2d600aa6e1bb1
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-01 11:03:18 +0200

    Match app -> dir.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/static/views.py
+++ musicdb/static/views.py
@@ -4,4 +4,4 @@ from musicdb.auth.decorators import login_not_required
 
 @login_not_required
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'static/index.html')
