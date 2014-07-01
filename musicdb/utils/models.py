15fda2918536c7880fec002956b36ef0e27a5ed0
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-01 11:13:22 +0200

    DRY
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/utils/models.py
+++ musicdb/utils/models.py
@@ -1,4 +1,7 @@
 from django.template import add_to_builtins
 
-add_to_builtins('musicdb.utils.templatetags.fonts')
-add_to_builtins('musicdb.utils.templatetags.media')
+for x in (
+    'fonts',
+    'media',
+):
+    add_to_builtins('musicdb.utils.templatetags.%s' % x)
