7f58215725132d9526b65fa9911567b8ae09cad1
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-01 15:02:50 +0200

    No need for this page really.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/static/views.py
+++ musicdb/static/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 from musicdb.auth.decorators import login_not_required
 
-@login_not_required
 def index(request):
     return render(request, 'static/index.html')
