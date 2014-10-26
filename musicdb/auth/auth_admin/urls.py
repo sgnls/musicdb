24241f46f7e190f20b5492265e6ced90aa36b55b
Chris Lamb <chris@chris-lamb.co.uk>
2014-10-26 20:33:09 +0000

    Can setup an email address.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/auth/auth_admin/urls.py
+++ musicdb/auth/auth_admin/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('musicdb.auth.auth_admin.views',
     url(r'^superuser/users$', 'view',
         name='view'),
+    url(r'^superuser/users/(?P<user_id>\d+)$', 'user',
+        name='user'),
 )
--- musicdb/auth/auth_admin/views.py
+++ musicdb/auth/auth_admin/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 
 from musicdb.utils.paginator import AutoPaginator
 from musicdb.utils.decorators import superuser_required
+
+from .forms import UserForm
 
 @superuser_required
 def view(request):
@@ -10,4 +13,24 @@ def view(request):
 
     return render(request, 'auth/admin/view.html', {
         'page': page,
+    })
+
+@superuser_required
+def user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User saved.")
+
+            return redirect('auth:admin:user', user.pk)
+    else:
+        form = UserForm(instance=user)
+
+    return render(request, 'auth/admin/user.html', {
+        'user': user,
+        'form': form,
     })
