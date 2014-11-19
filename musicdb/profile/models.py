bb39acdf5ef07b4623c83eaa7a4da89f52b655bf
Chris Lamb <chris@chris-lamb.co.uk>
2014-11-19 14:00:07 +0000

    Drop prefix field.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/profile/models.py
+++ musicdb/profile/models.py
@@ -5,10 +5,4 @@ from django.db import models
 from musicdb.utils.user_data import PerUserData
 
 class Profile(PerUserData('profile')):
-    prefix = models.CharField(
-        blank=True,
-        max_length=500,
-        verbose_name="Override file location prefix",
-    )
-
     kindle_email_address = models.EmailField(blank=True)
