2d33e5110c32ad046cf8a3ed9f8ccd8e3b1f942e
Chris Lamb <chris@chris-lamb.co.uk>
2014-11-19 12:28:10 +0000

    Send an email?
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/auth/auth_admin/forms.py
+++ musicdb/auth/auth_admin/forms.py
@@ -1,3 +1,5 @@
+from email_from_template import send_mail
+
 from django import forms
 from django.utils.crypto import get_random_string
 from django.contrib.auth.models import User
@@ -16,6 +18,11 @@ class NewUserForm(forms.ModelForm):
         instance = super(NewUserForm, self).save(commit=False)
         instance.set_password(password)
         instance.save()
+
+        send_mail((instance.email,), 'auth/admin/new_user.email', {
+            'user': instance,
+            'password': password,
+        })
 
         return instance
 
