dd185e64e6a4c0886ad516670c02147262c58d1a
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-02 12:12:26 +0000

    This too?

--- musicdb/nonclassical/management/commands/add_cd.py
+++ musicdb/nonclassical/management/commands/add_cd.py
@@ -85,6 +85,7 @@ class Command(AddMusicFilesCommand):
         self.copy_and_tag(files, 'albums/%d' % cd.pk, 'track', cd.tracks)
 
         # To ensure the slug?
+        artist.save()
         album.save()
 
         print "I: Added: %s%s" % (settings.SITE_URL, album.get_absolute_url())
