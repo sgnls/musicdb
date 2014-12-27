069d847ad0a1d972cb05067a5f81ea09a3b604e9
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-27 08:43:11 +0000

    Drop "google" here
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/utils/templatetags/fonts.py
+++ musicdb/utils/templatetags/fonts.py
@@ -3,11 +3,11 @@ from django.conf import settings
 
 register = template.Library()
 
-GOOGLE_FONT_TEMPLATE = '<link href="//fonts.googleapis.com/css?family=%s" rel="stylesheet">'
+TEMPLATE = '<link href="//fonts.googleapis.com/css?family=%s" rel="stylesheet">'
 
 @register.simple_tag
-def google_font(definition):
+def font(definition):
     if not settings.FONTS_ENABLED:
         return ''
 
-    return GOOGLE_FONT_TEMPLATE % definition
+    return TEMPLATE % definition
