9b0cab1a3380116f25ab491818cbc03f248aefe6
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-27 08:42:21 +0000

    Drop common stats.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/common/urls.py
+++ musicdb/common/urls.py
@@ -3,6 +3,4 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('musicdb.common.views',
     url(r'^common/play/(?P<music_file_id>\d+)$', 'play_music_file',
         name='play-music-file'),
-    url(r'^common/stats$', 'stats',
-        name='stats'),
 )
--- musicdb/common/views.py
+++ musicdb/common/views.py
@@ -1,5 +1,4 @@
-from django.db import models
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 
 from musicdb.utils.http import render_playlist
 
@@ -9,13 +8,3 @@ def play_music_file(request, music_file_id):
     music_file = get_object_or_404(MusicFile, pk=music_file_id)
 
     return render_playlist(request, [music_file])
-
-def stats(request):
-    total_duration = MusicFile.objects.aggregate(
-        x=models.Sum('length'),
-    )['x'] or 0
-
-    return render(request, 'common/stats.html', {
-        'total_duration': total_duration,
-        'music_file_count': MusicFile.objects.count(),
-    })
