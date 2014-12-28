526bf4b641020e207a180c22bc3febbc11f31bab
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-28 09:46:41 +0000

    Letters.

--- musicdb/books/managers.py
+++ musicdb/books/managers.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class AuthorManager(models.Manager):
     def letters(self):
-        return self.values_list('last_name_first', flat=True) \
-            .order_by('last_name_first').distinct()
+        return self.values_list(
+            'last_name_first', flat=True,
+        ).order_by(
+            'last_name_first',
+        ).distinct()
