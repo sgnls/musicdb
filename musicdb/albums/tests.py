015fba0892351653b745a760b9b640d2cfcb3f48
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-28 10:03:27 +0000

    Test artist
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- musicdb/albums/tests.py
+++ musicdb/albums/tests.py
@@ -1,6 +1,18 @@
 from musicdb.utils.test import TestCase
 
+from .models import Artist
+
 class SmokeTests(TestCase):
+    def setUp(self):
+        super(SmokeTests, self).setUp()
+
+        Artist.objects.create(name="Test artist")
+
     def test_view(self):
         self.assertHTTP302('albums:view')
         self.assertHTTP200('albums:view', 'a')
+
+    def test_artist(self):
+        artist = Artist.objects.get()
+
+        self.assertHTTP200(artist.get_absolute_url())
