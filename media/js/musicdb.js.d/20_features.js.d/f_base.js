6b48d76124c4b5c807d5fa8e24611e8ff4862cdd
Chris Lamb <chris@chris-lamb.co.uk>
2015-08-11 10:27:26 +0200

    Move back to regular bootstrap
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- media/js/musicdb.js.d/20_features.js.d/f_base.js
+++ media/js/musicdb.js.d/20_features.js.d/f_base.js
@@ -1,15 +1,2 @@
 $.feature('f_base', function () {
-  var fixUnfixNavbar = function() {
-    var toggle = ($(window).scrollTop() > 40);
-
-    $('body').toggleClass('f_fixed_top', toggle);
-    $('#header').toggleClass('navbar-fixed-top', toggle);
-  };
-
-  $(window).bind('scroll', function() {
-    fixUnfixNavbar();
-    return true;
-  });
-
-  fixUnfixNavbar();
 });
