9391deba4f73016edf809e93fa4df77d9ff2577c
Chris Lamb <chris@chris-lamb.co.uk>
2015-08-14 20:00:43 +0200

    rawstrings

--- musicdb/utils/track_names.py
+++ musicdb/utils/track_names.py
@@ -3,8 +3,8 @@ import re
 
 def track_names_from_filenames(filenames, capitalise=True):
     filenames = [os.path.basename(x) for x in filenames]
-    filenames = [re.sub('\.[^\.]+$', '', x) for x in filenames]
-    filenames = [re.sub('^[\d_\-]+', '', x) for x in filenames]
+    filenames = [re.sub(r'\.[^\.]+$', '', x) for x in filenames]
+    filenames = [re.sub(r'^[\d_\-]+', '', x) for x in filenames]
     filenames = [x.replace('[', '(') for x in filenames]
     filenames = [x.replace(']', ')') for x in filenames]
 
@@ -16,15 +16,15 @@ def track_names_from_filenames(filenames, capitalise=True):
     if len(common_suffix) >= 3:
         filenames = [x[:-len(common_suffix)] for x in filenames]
 
-    filenames = [re.sub('^[\d_\.\-\s]+', '', x) for x in filenames]
-    filenames = [re.sub('^[VI]+\.\s*', '', x) for x in filenames]
-    filenames = [re.sub('[_\s]+', ' ', x) for x in filenames]
+    filenames = [re.sub(r'^[\d_\.\-\s]+', '', x) for x in filenames]
+    filenames = [re.sub(r'^[VI]+\.\s*', '', x) for x in filenames]
+    filenames = [re.sub(r'[_\s]+', ' ', x) for x in filenames]
 
     filenames = [x or '(unknown)' for x in filenames]
     filenames = [x[0].capitalize() + x[1:] for x in filenames]
     filenames = [re.sub(r'\b- ', ': ', x) for x in filenames]
 
-    filenames = [re.sub('\s+\((19|20)\d\d\)$', '', x) for x in filenames]
+    filenames = [re.sub(r'\s+\((19|20)\d\d\)$', '', x) for x in filenames]
 
     if capitalise:
         re_capital = re.compile(r'(^|\s|\()(\w)')
