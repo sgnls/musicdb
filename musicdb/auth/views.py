35f4888f183751551aee78936a612cbddee5f71c
Chris Lamb <chris@chris-lamb.co.uk>
2015-08-14 20:00:54 +0200

    Drop unused import.

--- musicdb/auth/views.py
+++ musicdb/auth/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 
-from .decorators import login_not_required
-
 def login(request):
     if request.user.is_authenticated():
         return redirect('static:landing')
