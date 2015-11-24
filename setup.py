cfdf7b5846dc9309d23ab5f9ce2300f93899894c
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-24 18:21:59 +0200

    WIP on setuptest/coveralls, etc.

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ import sys
 from os.path import join, dirname, abspath
 from setuptools import setup, find_packages
 
+sys.path.insert(0, join(dirname(abspath(__file__)), 'contrib'))
+
 def find_data_files(dirs):
     result = []
     for x in dirs:
@@ -16,16 +18,11 @@ def find_data_files(dirs):
             ))
     return result
 
-if sys.argv[1:2] == ['test']:
-    sys.path.insert(0, join(dirname(abspath(__file__)), 'contrib'))
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicdb.settings')
-    from django.core.management import execute_from_command_line
-    sys.exit(execute_from_command_line(sys.argv + ['--verbosity=2']))
-
 setup(
     name='musicdb',
     scripts=('musicdb/manage.py',),
     packages=find_packages(),
     zip_safe=False,
     data_files=find_data_files(('media', 'templates')),
+    test_suite='setuptest.setuptest.SetupTestSuite',
 )
